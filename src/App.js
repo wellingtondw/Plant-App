9dad802564b796cc7f17e1734e5362ae03348ab8
wellington <wellingtonslpbi@hotmail.com>
2020-03-04 21:18:30 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Plant App</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Image} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Welcome from '../screens/Welcome';
+import Login from '../screens/Login';
+import Explore from '../screens/Explore';
+import Browse from '../screens/Browse';
+import Product from '../screens/Product';
+import Settings from '../screens/Settings';
+
+import {theme} from '../constants';
+
+const screens = createStackNavigator(
+  {
+    Welcome,
+    Login,
+    Explore,
+    Browse,
+    Product,
+    Settings,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {},
+      headerBackImage: <Image />,
+      headerBackTitle: null,
+      headerLeftContainerStyle: {},
+      headerRightContainerStyle: {},
+    },
+  },
+);
+
+export default createAppContainer(screens);
